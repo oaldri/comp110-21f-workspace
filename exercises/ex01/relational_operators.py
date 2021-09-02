4711207ffcd5af4043bde3653d3b672b1d5be9cc
Olivia Aldridge <oaldri@ad.unc.edu>
2021-09-02 17:40:35 -0400

    before ex02

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -4,7 +4,7 @@ __author__ = "730383481"
 left_hand_side: str = input("left hand side. ")
 right_hand_side: str = input("right hand side. ")
 
-print(left_hand_side + " < " + right_hand_side + " is " + str(left_hand_side < right_hand_side))
-print(left_hand_side + " >= " + right_hand_side + " is " + str(left_hand_side >= right_hand_side))
-print(left_hand_side + " ==" + right_hand_side + " is " + str(left_hand_side == right_hand_side))
+print(left_hand_side + " < " + right_hand_side + " is " + str(int(left_hand_side) < int(right_hand_side)))
+print(left_hand_side + " >= " + right_hand_side + " is " + str(int(left_hand_side) >= int(right_hand_side)))
+print(left_hand_side + " == " + right_hand_side + " is " + str(left_hand_side == right_hand_side))
 print(left_hand_side + " != " + right_hand_side + " is " + str(left_hand_side != right_hand_side))
