066c61f6aa1ffe858f7401b0fc5cc10b00a38cd9
Olivia Aldridge <oaldri@ad.unc.edu>
2021-09-13 13:48:50 -0400

    updates

---
+++ sandbox/3.py
@@ -0,0 +1,16 @@
+"""Plz work I fucking hate this class so goddamn much why red squiggly line just work."""
+
+__author__ = "730383481"
+
+
+choice: int = int(str("Choose a number.")
+
+if: choice % 2 == 0 and choice % 7 == 0
+    print("TAR HEELS")
+    else:
+        if: choice % 2 == 0 
+        print("TAR")
+        if: choice % 7 == 0 
+        print("Heels")
+else:
+    print("CAROLINA")
